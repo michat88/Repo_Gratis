3c12a24ea346883c09482e1666914dfc956d3713
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-02-11 04:17:17 +0900

    Update JavHeyPlugin.kt

--- JavHey/src/main/kotlin/com/JavHey/JavHeyPlugin.kt
+++ JavHey/src/main/kotlin/com/JavHey/JavHeyPlugin.kt
@@ -7,6 +7,13 @@ import android.content.Context
 @CloudstreamPlugin
 class JavHeyPlugin: Plugin() {
     override fun load(context: Context) {
+        // 1. Daftarkan Provider Utama (Situs JavHey)
         registerMainAPI(JavHey())
+
+        // 2. Daftarkan Extractor (Server Video)
+        // Ini wajib agar saat aplikasi menemukan link "hglink.to" atau "haxloppd.com",
+        // ia tahu harus menggunakan logika di file Extractor.kt
+        registerExtractorAPI(Hglink())
+        registerExtractorAPI(Haxloppd())
     }
 }
